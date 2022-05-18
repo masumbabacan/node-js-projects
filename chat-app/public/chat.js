8f66dfa90dbce333e52c3e5656cbf4f9768e4b40
Masum Babacan <mmasumbabacan@gmail.com>
2022-05-18 18:29:44 +0300

    new

---
+++ chat-app/public/chat.js
@@ -0,0 +1,33 @@
+const socket = io.connect("http://localhost:3000");
+const sender = document.getElementById("sender");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submitBtn");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
+
+submitBtn.addEventListener('click',() => {
+    socket.emit("chat", {
+        message : message.value,
+        sender: sender.value
+    })
+})
+
+socket.on("chat", data => {
+    feedback.innerHTML = "";
+    output.innerHTML += `<p><strong>${data.sender} : </strong> ${data.message}</p>`;
+    message.value = "";
+})
+
+message.addEventListener("keypress",() => {
+    socket.emit('typing',sender.value);
+})
+
+socket.on("typing",data => {
+    feedback.innerHTML = `<p style="margin:0 10px;">${data} yazıyor...</p>`;
+    setTimeout(() => {
+        feedback.innerHTML = "";
+    }, 2000);
+})
+
+
+
